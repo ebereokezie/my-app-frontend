5f2ce962f4d3d76a6acb1f37e4a8169ee076e275
Ebere Okezie <ebereokezie@gmail.com>
2022-10-04 09:43:08 -0500

    add style to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "25px",
+    margin: "0 6px 6px",
+    color: "white",
+  };
+
 function NavBar(){
     return (
         <div className = "NavBar">
-            <NavLink to = "/"> Home </NavLink>
-            <NavLink to = "/"> Monday </NavLink>
-            <NavLink to = "/"> Tuesday </NavLink>
-            <NavLink to = "/"> Wednesday </NavLink>
-            <NavLink to = "/"> Thursday </NavLink>
-            <NavLink to = "/"> Friday </NavLink>
-            <NavLink to = "/"> Saturday </NavLink>
+            <NavLink to = "/" exact style = {linkStyles}> Home </NavLink>
+            <NavLink to = "/Monday" exact style = {linkStyles}> Monday </NavLink>
+            <NavLink to = "/Tuesday" exact style = {linkStyles}> Tuesday </NavLink>
+            <NavLink to = "/Wednesday" exact style = {linkStyles}> Wednesday </NavLink>
+            <NavLink to = "/Thursday" exact style = {linkStyles}> Thursday </NavLink>
+            <NavLink to = "/Friday" exact style = {linkStyles}> Friday </NavLink>
+            <NavLink to = "/Saturday" exact style = {linkStyles}> Saturday </NavLink>
+            <NavLink to = "/Sunday" exact style = {linkStyles}> Sunday </NavLink>
+
 
         </div>
     )
