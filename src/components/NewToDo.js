08f40c08d1daf49f9bef47b2f08006dd8226f240
Ebere Okezie <ebereokezie@gmail.com>
2022-10-04 15:58:17 -0500

    update New ToDo

--- src/components/NewToDo.js
+++ src/components/NewToDo.js
@@ -1,6 +1,39 @@
-import React from "react"
+import React, { useState} from "react"
 
-function NewToDo({})
+function NewToDo({onAddToDo}) {
+    const [toDoBody, setToDoBody] = useState("")
+
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    
+    //     fetch("http://localhost:4000/messages", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({
+    //         body: toDoBody,
+    //       }),
+    //     })
+    //       .then((r) => r.json())
+    //       .then((newToDo) => {
+    //         onAddToDo(newToDo);
+    //         setToDoBody("");
+    //       });
+
+return (
+    <form className = "new-todo" onSubmit ={handleSubmit}>
+        <input
+        type="text"
+        name="body"
+        value={toDoBody}
+        onChange={(e) => setToDoBody(e.target.value)}
+        />
+        <button type="submit">Update List</button>
+    </form>
+);
+
+}
 
 
 export default NewToDo
