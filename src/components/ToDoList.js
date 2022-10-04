4b998b70a9a14d6bf9665bf4c78b0be4bace9210
Ebere Okezie <ebereokezie@gmail.com>
2022-10-04 14:54:54 -0500

    add to do to ToDoList component

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ToDo from "./ToDo"
 
-function ToDoList({})
+function ToDoList({
+    toDos,
+    onDeleteToDo,
+    onUpdateToDo
+}) {
+    return (
+        <div className ="toDoList">
+            <li>
+                {toDos.map((toDo) => (
+                    <ToDo
+                        key={toDo.id}
+                        toDo={toDo}
+                        onDeleteToDo={onDeleteToDo}
+                        onUpdateToDo={onUpdateToDo}
+                    />
+                ))}
+            </li>
+        </div>
+    )
+}
 
 export default ToDoList
