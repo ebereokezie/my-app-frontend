9bf3e5de6b44f6e6b420f5c7e56b95327c4cc86c
Ebere Okezie <ebereokezie@gmail.com>
2022-10-05 21:25:46 -0500

    remove navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {Route, Switch} from "react-router-dom";
+import React from "react"
 import Home from "./Home";
-import NavBar from './NavBar';
-import Monday from './Monday';
-import Tuesday from './Tuesday';
-import Wednesday from './Wednesday';
-import Thursday from './Thursday';
-import Friday from './Friday';
-import Saturday from './Saturday';
-import Sunday from './Sunday';
 
 
 function App() {
@@ -16,34 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
-        <Switch>
-          <Route exact path = "/">
             <Home/>
-          </Route>
-          <Route exact path = "/Monday">
-            <Monday />
-          </Route>
-          <Route exact path = "/Tuesday">
-            <Tuesday />
-          </Route>
-          <Route exact path = "/Wednesday">
-            <Wednesday />
-          </Route>
-          <Route exact path = "/Thursday">
-            <Thursday />
-          </Route>
-          <Route exact path = "/Friday">
-            <Friday />
-          </Route>
-          <Route exact path = "/Saturday">
-            <Saturday />
-          </Route>
-          <Route exact path = "/Sunday">
-            <Sunday />
-          </Route>
-        </Switch>
-      
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -32,12 +32,9 @@ function Game({game, onDeleteGame, onUpdateGame}) {
             <p>Title: {game.title}</p>
             <p>Platform: {game.platform}</p>
             <p>Price:  ${game.price}</p>
-            <p>Review</p>
+            <p>Review:</p>
             {game.reviews.map((review) => (
-        <div>
-          <p>Score: {review.score}</p>
-          <p>Comment: {review.comment}</p>
-        </div>
+              <p>Score: {review.score}</p>
       ))}
             </li>
               
